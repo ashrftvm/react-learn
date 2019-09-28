695f69bb0e55dc91ecd774b5b4092d68b39a65e5
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-28 16:39:45 +0400

    component creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // let's go!
+import React from "react";
+import StorePicker from "./components/StorePicker";
+import { render } from "react-dom";
+
+render(<StorePicker />, document.querySelector("#main"));
