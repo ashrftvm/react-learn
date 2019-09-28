67e0392befb353572199e54a7f3365dd7283248d
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-28 17:03:24 +0400

    props to send from one component to another

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="One Liner Catch" />
         </div>
         <Order />
         <Inventory />
