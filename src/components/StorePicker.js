07ba18b243f96d5c8987d3a6257f1b985f5e778f
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-28 17:52:38 +0400

    bind diff function to the class, send a form data

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,9 +2,25 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log("You are trying to change the URL");
+    console.log(this.storeInput.value);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      // <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+      <form
+        className="store-selector"
+        onSubmit={e => {
+          this.goToStore(e);
+        }}
+      >
         {/* Comments can only be done like this */}
         <h2>Please Enter a Store</h2>
         <input
@@ -12,6 +28,7 @@ class StorePicker extends React.Component {
           required
           placeholder="Store Name"
           defaultValue={getFunName()}
+          ref={input => (this.storeInput = input)}
         />
         <button type="submit">Visit Store ➡️</button>
       </form>
